67c0cbaa70ae9d19dd4b60863bb9a81d9374c668
Alinaand <80483769+Alinaand@users.noreply.github.com>
2022-02-20 21:45:17 +0200

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const imageGallery = document.querySelector('.gallery');
+const imageItems = createGalleryItems(galleryItems);
+imageGallery.insertAdjacentHTML('beforeend',imageItems);
+console.log(imageGallery);
+function createGalleryItems(items){
+    return galleryItems.map(({preview,original,description})=>
+    {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>`
+    }).join('');
+};
+console.log(galleryItems);
+let gallery = new SimpleLightbox('.gallery a',
+{captions:true,
+captionsData:'alt',
+captionDelay: 250,});
 
 console.log(galleryItems);
